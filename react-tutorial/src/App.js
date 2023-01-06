3e7e14a1611c1213a5fec494acd4869e0d939037
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-06 14:12:32 +0900

    完了したタスクを削除する

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -5,10 +5,17 @@ import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [todos,setTodos] = useState([
-    {id:1,name:"Todo1",completed:false},
+   
   ]);
 
 const todoNameRef = useRef();
+
+const toggleTodo = (id)=>{
+  const newTodos =[...todos];
+  const todo = newTodos.find((todo)=>todo.id ===id);
+  todo.conpleted = !todo.completed;
+  setTodos(newTodos);
+}
 
 const handleAppTodo = () =>{
   //タスクを追加するする
@@ -19,12 +26,16 @@ setTodos((prevTodos)=>{
 todoNameRef.current.value = null;
 };
 
+const handleClear = ()=>{
+  const newTodos = todos.filter((todo)=>!todo.conpleted);
+  setTodos(newTodos);
+};
   return<div>
-<TodoList todos={todos}/>
+<TodoList todos={todos} toggleTodo={toggleTodo}/>
 <input type="text" ref={todoNameRef}/>
 <button onClick={handleAppTodo}>タスクを追加</button>
-<button>完了したタスクの削除</button>
-<div>残りのタスク:0</div>
+<button onClick={handleClear}>完了したタスクの削除</button>
+<div>残りのタスク:{todos.filter((todo)=>!todo.conpleted).length}</div>
   </div>
 }
 
--- react-tutorial/src/Todo.js
+++ react-tutorial/src/Todo.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-export const Todo = ({todo}) => {
+
+export const Todo = ({todo,toggleTodo}) => {
+  const handleTodoClick = ()=>{
+    toggleTodo(todo.id)
+  }
   return (
     <div>
       <label>
-        <input type="checkbox" checked={todo.completed} readOnly></input>
+        <input type="checkbox" checked={todo.completed} readOnly onChange={handleTodoClick}></input>
       </label>
       {todo.name}
       </div>
