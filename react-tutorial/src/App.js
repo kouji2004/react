c75afeb08539687dee518ac5832e6f27e9b3ec6a
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-06 11:38:09 +0900

    タスクの追加

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -4,6 +4,10 @@ import TodoList from './TodoList';
 function App() {
   return<div>
 <TodoList/>
+<input type="text"/>
+<button>タスクを追加</button>
+<button>完了したタスクの削除</button>
+<div>残りのタスク:0</div>
   </div>
 }
 
